ee55496daa5ae3f759c36c798e6dcb3e3cd70205
Helder Gonçalo Oliveira <heldergfao@gmail.com>
2025-08-25 07:05:45 +0100

    Initial Poisson model, lacks documentation

--- src/app/data/utils.py
+++ src/app/data/utils.py
@@ -4,6 +4,7 @@ BASE_DIR = Path(__file__).parent.absolute()
 
 DIRTY_PATH = BASE_DIR / "past" / "dirty"
 CLEAN_PATH = BASE_DIR / "past" / "clean"
+OUTPUT_PATH = BASE_DIR / "output"
 
 BASE_COLUMNS = [
     "Div",
@@ -30,3 +31,15 @@ BASE_COLUMNS = [
     "HR",
     "AR"
 ]
+
+SEASONS = [
+    "19_20",
+    "20_21",
+    "21_22",
+    "22_23",
+    "23_24",
+    "24_25"
+]
+
+POISSON_LEARNING_BIAS = 0.1
+POISSON_EWMA = 0.2
