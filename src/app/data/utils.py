fc6e543f9b56601de35bdd1411b9cd993eec7342
Helder Oliveira <heldergfao@gmail.com>
2025-11-12 17:17:06 +0000

    Revamped Poisson

--- src/app/data/utils.py
+++ src/app/data/utils.py
@@ -7,6 +7,10 @@ CLEAN_PATH = BASE_DIR / "past" / "clean"
 OUTPUT_PATH = BASE_DIR / "output" / "poisson"
 OUTPUT_PATH_REVAMPED = BASE_DIR / "output" / "poisson_revamped"
 
+USED_MODEL = "poisson_revamped"
+
+LEAGUE_ID = "P1"
+
 BASE_COLUMNS = [
     "Div",
     "Date",
@@ -43,12 +47,19 @@ SEASONS = [
     "25_26"
 ]
 
-POISSON_LEARNING_BIAS = 0.1
-POISSON_EWMA = 0.2
+POISSON_LEARNING_BIAS = 0.05
+POISSON_EWMA = 0.05
+POISSON_OD = 1.05
 
-POISSON_REVAMPED_LEARNING_BIAS = 0.12
-POISSON_REVAMPED_EWMA = 0.15
-POISSON_REVAMPED_OD = 1.05
 
-LEAGUE_AVG_SCORED = 1.4
+# Médias fallback para o modelo de GOLOS (baseadas no teu utils antigo)
+LEAGUE_AVG_SCORED = 1.4 
 LEAGUE_AVG_AGAINST = 1.2
+
+
+# Deves ter também os teus parâmetros otimizados anteriores
+POISSON_REVAMPED_LEARNING_BIAS = 0.05
+POISSON_REVAMPED_EWMA = 0.05
+POISSON_REVAMPED_OD = 1.07
+POISSON_REVAMPED_RF_WEIGHT = 0.3
+POISSON_REVAMPED_SEASON_WEIGHT = 0.325
